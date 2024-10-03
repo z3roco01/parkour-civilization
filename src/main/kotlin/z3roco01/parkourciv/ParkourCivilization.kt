e886e56c8ee2ab51e2096c13b27a990fd4caf84e
zerocool <46464404+z3roco01@users.noreply.github.com>
2024-10-03 17:18:56 -0500

    start creating the gen

--- src/main/kotlin/z3roco01/parkourciv/ParkourCivilization.kt
+++ src/main/kotlin/z3roco01/parkourciv/ParkourCivilization.kt
@@ -1,6 +1,7 @@
 package z3roco01.parkourciv
 
 import net.fabricmc.api.ModInitializer
+import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
 object ParkourCivilization : ModInitializer {
@@ -12,4 +13,11 @@ object ParkourCivilization : ModInitializer {
 
 		logger.info("init done !!")
 	}
+
+	/**
+	 * makes an id with the path specified
+	 * @param path the path of the [Identifier]
+	 * @return the fully qualified [Identifier]
+	 */
+	fun mkId(path: String) = Identifier.of(MOD_ID, path)
 }
